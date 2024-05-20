acdc584c5dbcad15973ed04a5f172c8087313983
Aigerim Saparbaeva <joymagritte@gmail.com>
2024-05-20 06:57:48 +0600

    level 2 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import './App.css'
+import './App.css';
 import Card from "./components/Card/Card.tsx";
 
 const App = () => {
+ const [hand, setHand] = useState<Card[]>([]);
 
+ const dealCards = () =>  {
+      const deck = new CardDeck();
+      const newHand = deck.getCards(5);
+      setHand(newHand);
+ };
+ let content;
+ if (hand.length === 0) {
+     content = (
+         <button onClick={dealCards}>Раздать карты</button>
+     );
+ } else  {
+     content = (
+         <>
+             {hand.map((card:string) => (
+                 <Card key={card.id} rank ={card.rank} suit={card.suit}/>
+             ))}
+             <button onClick ={dealCards}>Раздать карты</button>
+         </>
+     );
+ }
   return (
       <div className="playingCards faceImages">
-          <Card rank={"K"} suit={"diams"}/>
+          {content}
       </div>
-)
+);
 };
 
-export default App
+export default App;
